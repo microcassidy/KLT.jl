cf02cfff5e6758c6d6a40a0dfc5a50a28b11b13d
Michael Cassidy <mc.mathematics@gmail.com>
2026-04-15 21:03:07 +1000

    initial commit

--- src/KLT.jl
+++ src/KLT.jl
@@ -1,5 +1,12 @@
 module KLT
-
+using StatsBase
+using LinearAlgebra
 # Write your package code here.
+export klt
+
+function klt(X::AbstractMatrix{Float64},dims::Int=2)
+    K = Hermitian(cov(X,dims=dims))
+    eigen(K,sortby = λ -> -real(λ)) #descending order
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,8 @@ using KLT
 using Test
 
 @testset "KLT.jl" begin
-    # Write your tests here.
+    X = rand(9,100)
+    k = klt(X)
+    @test issorted(k.values,by=x->-x)
+    @test size(k.vectors) == (9,9)
 end
